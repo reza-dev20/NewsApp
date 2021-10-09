0fd65d6457c5637f44fac6878070a3df96b936e1
reza-dev20 <reza.dev20@gmail.com>
2021-10-09 17:41:21 +0330

    create db classes(dao,database class,...) and add db operation methods to repository and viewModel

--- app/src/main/java/com/example/newsApp/repository/NewsRepository.kt
+++ app/src/main/java/com/example/newsApp/repository/NewsRepository.kt
@@ -1,13 +1,27 @@
 package com.example.newsApp.repository
 
 import com.example.newsApp.api.RetrofitInstance
+import com.example.newsApp.db.ArticleDao
+import com.example.newsApp.db.ArticleDatabase
+import com.example.newsApp.models.Article
 
 class NewsRepository(
-
+    val db: ArticleDatabase
 ) {
+    private val dao: ArticleDao = db.getArticleDao()
+
     suspend fun getBreakingNews(countryCode: String , pageNumber: Int) =
         RetrofitInstance.api.getBreakingNews(countryCode , pageNumber)
 
     suspend fun searchNews(searchQuery: String , pagingNumber: Int) =
         RetrofitInstance.api.searchForNews(searchQuery , pagingNumber)
+
+    suspend fun upsert(article: Article)=
+        dao.upsert(article)
+
+    fun getSavedNews()=
+        dao.getAllArticles()
+
+    suspend fun deleteArticle(article: Article)=
+        dao.deleteArticle(article)
 }
--- app/src/main/java/com/example/newsApp/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/newsApp/ui/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.example.newsApp.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.newsApp.models.Article
 import com.example.newsApp.models.NewsResponse
 import com.example.newsApp.repository.NewsRepository
 import com.example.newsApp.util.Resource
@@ -58,6 +59,16 @@ class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
             }
         }
         return Resource.Error(response.message())
+    }
+
+    fun saveArticle(article: Article)= viewModelScope.launch {
+        newsRepository.upsert(article)
+    }
+
+    fun getSavedNews()=newsRepository.getSavedNews()
+
+    fun deleteArticle(article: Article)=viewModelScope.launch {
+        newsRepository.deleteArticle(article)
     }
 
 }
