52f51b97786f65cd70c653119395155642e01838
reza-dev20 <reza.dev20@gmail.com>
2021-10-09 14:41:31 +0330

    Packaging

--- app/src/main/java/com/example/newsApp/ui/MainActivity.kt
+++ app/src/main/java/com/example/newsApp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.newsApp
+package com.example.newsApp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsApp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
