c4eb84696a6c77d41bbbc14a9f3dd1c0b8c39e90
reza-dev20 <reza.dev20@gmail.com>
2021-10-10 01:48:49 +0330

    coding for fragment layout(xml-ui design)

--- app/src/main/java/com/example/newsApp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsApp/ui/NewsActivity.kt
@@ -2,11 +2,25 @@ package com.example.newsApp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.newsApp.R
+import com.example.newsApp.db.ArticleDatabase
+import com.example.newsApp.repository.NewsRepository
+import kotlinx.android.synthetic.main.activity_main.*
 
 class NewsActivity : AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory=NewsViewModelProviderFactory(newsRepository)
+        viewModel= ViewModelProvider(this,viewModelProviderFactory).get(NewsViewModel::class.java)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
     }
 }
